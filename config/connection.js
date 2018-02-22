022ef8679ef85822cba578935df3c1e5917c016b
Matt McIntyre <mgmcintyre777@gmail.com>
2018-02-22 11:38:20 -0500

    final

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,10 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection 
 
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 
 connection.connect(function(err) {
   if (err) {
